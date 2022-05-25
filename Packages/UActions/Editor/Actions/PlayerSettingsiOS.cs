984b452b720d41fd7c176dbceadd433110efed95
aaron <qkrsogusl3@gmail.com>
2022-05-25 16:32:51 +0900

    update PlayerSettingiOS

--- Packages/UActions/Editor/Actions/PlayerSettingsiOS.cs
+++ Packages/UActions/Editor/Actions/PlayerSettingsiOS.cs
@@ -1,14 +1,26 @@
 ﻿using System.Collections.Generic;
+using UnityEditor;
+using YamlDotNet.Core;
+using YamlDotNet.Serialization;
 
 namespace UActions.Editor.Actions
 {
     [Action("player-settings-ios")]
     public class PlayerSettingsiOS : IAction
     {
-        private readonly Dictionary<string, object> _with;
+        public class Registration : IRegistration
+        {
+            public void Register(DeserializerBuilder builder)
+            {
+                builder.WithTagMapping(new TagName("!target-sdk"), typeof(iOSSdkVersion));
+            }
+        }
+
         public TargetPlatform Targets => TargetPlatform.iOS;
+        private readonly Dictionary<string, object> _with;
 
 
+        [Input("target-sdk", true, typeof(iOSSdkVersion))]
         public PlayerSettingsiOS(Dictionary<string, object> with)
         {
             _with = with;
@@ -16,6 +28,10 @@ namespace UActions.Editor.Actions
 
         public void Execute(WorkflowContext context)
         {
+            if (_with.TryGetIsValue("target-sdk", out iOSSdkVersion sdkVersion))
+            {
+                PlayerSettings.iOS.sdkVersion = iOSSdkVersion.DeviceSDK;
+            }
         }
     }
 }
